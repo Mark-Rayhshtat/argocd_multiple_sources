f80f399af68b02acb30a23d0d9072eda3e2b8c93
Mark-Rayhshtat <markr@comm-it.com>
2023-06-25 19:17:26 +0300

    build script 10

--- script.sh
+++ script.sh
@@ -2,10 +2,10 @@
 set -e
 echo "Create list of updated applications"
 echo "--------------------------------------------------------------"
-# aws codepipeline list-pipeline-executions --pipeline-name pipeline-deploy-service01-api-${env} --region eu-central-1
-aws codepipeline list-pipeline-executions --pipeline-name codepipeline-argocd-test --region us-east-1
+# aws codepipeline list-pipeline-executions --pipeline-name codepipeline-argocd-test --region us-east-1
 start_hash=$(aws codepipeline list-pipeline-executions --pipeline-name codepipeline-argocd-test --region us-east-1 --query 'pipelineExecutionSummaries[?(status==`InProgress` || status==`Succeeded`)]'| jq .[0].sourceRevisions[0].revisionId)
-end_hash= $(aws codepipeline list-pipeline-executions --pipeline-name codepipeline-argocd-test --region us-east-1 --query 'pipelineExecutionSummaries[?(status==`InProgress` || status==`Succeeded`)]'| jq .[1].sourceRevisions[0].revisionId)
+end_hash=$(aws codepipeline list-pipeline-executions --pipeline-name codepipeline-argocd-test --region us-east-1 --query 'pipelineExecutionSummaries[?(status==`InProgress` || status==`Succeeded`)]'| jq .[1].sourceRevisions[0].revisionId)
+echo "test"
 git diff --name-only $start_hash $end_hash > changes.log
 cat changes.log
 apps=$(python3 -c "with open('changes.log') as f:lines = set(line.split('/')[1] for line in f if line.split('/')[0] == 'helm' and len(line.split('/')) >= 3);[print(line) for line in lines]")
