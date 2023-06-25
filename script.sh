665b8f4805fd16f69841da5eb2660e9f5aaf6afa
Mark-Rayhshtat <markr@comm-it.com>
2023-06-25 18:59:03 +0300

    build script 5

--- script.sh
+++ script.sh
@@ -5,7 +5,7 @@ echo "--------------------------------------------------------------"
 # aws codepipeline list-pipeline-executions --pipeline-name pipeline-deploy-service01-api-${env} --region eu-central-1
 start_hash=$(aws codepipeline list-pipeline-executions --pipeline-name codepipeline-argocd-test --region us-east-1 --query 'pipelineExecutionSummaries[?(status==`InProgress` || status==`Succeeded`)]'| jq -r .[0].sourceRevisions[0].revisionId)
 echo $start_hash
-end_hash= $(aws codepipeline list-pipeline-executions --pipeline-name codepipeline-argocd-test --region us-east-1 --query 'pipelineExecutionSummaries[?(status==`InProgress` || status==`Succeeded`)]'| jq -r .[1].sourceRevisions[0].revisionId)
+end_hash=  $(aws codepipeline list-pipeline-executions --pipeline-name codepipeline-argocd-test --region us-east-1 --query 'pipelineExecutionSummaries[?(status==`InProgress` || status==`Succeeded`)]'| jq -r .[1].sourceRevisions[0].revisionId)
 echo $end_hash
 git diff --name-only $start_hash $end_hash > changes.log
 cat changes.log
